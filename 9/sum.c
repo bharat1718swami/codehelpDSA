68cc6435f511b326140407bdc833884b41463fb8
bharat <79807425+bharat1718swami@users.noreply.github.com>
2022-03-17 00:58:46 +0530

    Add files via upload

---
+++ 9/sum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    char name;
+    long long x=pow(2,63);
+  //  printf("%o\n",a);
+  //  printf("%x\n",a);
+  // printf("%lli\n",x);
+    //printf("Enter number  ");
+  // /// scanf("%d",&a);
+  a=15/6;
+  printf("%f",a);
+  a=(int)a;
+  printf("%d",a);
+  printf("%d",a);
+    printf("Enter character  ");
+    //scanf(" %d",&a);
+   // a=name;
+   for(int i=0;i<=400;i++){
+    printf("character is %c : %d\n",i,i);   
+    //printf("string is %c ",a); 
+   }
+    return 0;
+}
