5bcef2ad00a746f98f00a90851f389ed3c2168a1
bharat <79807425+bharat1718swami@users.noreply.github.com>
2022-06-13 23:38:17 +0530

    Add files via upload

---
+++ 5/countfor.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"enter a number\n";
+    cin>>n;
+    for ( i = 1; i <= n; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+return 0;
+}
