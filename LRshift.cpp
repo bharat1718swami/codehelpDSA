9653b7bf03e0b366c5eed027ec671380ad7b15a9
Bharat Swami <79807425+bharat1718swami@users.noreply.github.com>
2022-06-18 23:53:26 +0530

    Add files via upload

---
+++ LRshift.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"enter a number\n";
+  cin>>a;
+  cout<<(a>>1)<<endl;
+  cout<<(a>>2)<<endl;
+  cout<<(a>>3)<<endl;
+  cout<<(a<<1)<<endl;
+  cout<<(a<<2)<<endl;
+  cout<<(a<<3)<<endl;
+return 0;
+}
